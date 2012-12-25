89e61745d63125cfd7cc28fdb84b69eb4991095b
Harri Siirak <harri.siirak@gmail.com>
2012-12-25 22:55:09 +0200

    Message send support

--- test/client.test.js
+++ test/client.test.js
@@ -1,13 +1,28 @@
 var config = require('easy-config');
 var test = require('tap').test;
-var Client = require('../lib/client');
+var messente = require('../lib/client');
 
 // Globals
 var client = null;
 
 test('create a new client', function(t) {
-  client = new Client(config);
+  messente.createClient(config, function(err, cli) {
+    t.ifError(err, 'Messente client creation error');
+    t.ok(cli, 'Messente client created');
 
-  t.ok(client, 'client created');
-  t.end();
+    client = cli;
+    t.end();
+  });
+});
+
+test('sending a message', function(t) {
+  var opts = {
+    to: config.phones,
+    text: 'Hello!'
+  };
+
+  client.sendMessage(opts, function(err, result) {
+    console.log(arguments);
+    t.end();
+  });
 });
