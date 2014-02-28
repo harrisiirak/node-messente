3a544a7a62acdf2eedf18b674a19d9c04330188b
Harri Siirak <harri.siirak@gmail.com>
2014-02-28 22:16:08 +0200

    Convert tests from tap to mocha. Closes #3

---
+++ test/client.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var demand = require('must');
+var messente = require('../lib/client');
+
+describe('Messente API client', function () {
+  var client = null;
+  var ids = [];
+
+  var numbers = process.env.MSNTE_NUMBERS.split(',') || [];
+  var opts = {
+    secure: process.env.MSNTE_SECURE ? process.env.MSNTE_SECURE : true,
+    username: process.env.MSNTE_USER || null,
+    password: process.env.MSNTE_PWD || null
+  };
+
+  it('should create a new client', function (done) {
+    messente.createClient(opts, function(err, _client) {
+      demand(err).be.null();
+      demand(_client).be.object();
+
+      client = _client;
+      done();
+    });
+  });
+
+  it('should send a message', function (done) {
+    client.sendMessage({ text: 'Hello!', to: numbers }, function onFinish (err, result, _ids) {
+      demand(err).be.null();
+      demand(result).be.object();
+      demand(_ids).be.array();
+      demand(_ids).length(1);
+
+      ids = _ids;
+      done();
+    });
+  });
+
+  // NOTE: Messente 'get_dlr_response' API call seems to be broken
+  it('should get message report', function (done) {
+    client.getReport(ids, function onFinish (err, result) {
+      demand(err).be.null();
+      demand(result).be.object();
+
+      done();
+    });
+  });
+
+  it('should get account balance', function (done) {
+    client.getAccountBalance(function onFinish (err, balance) {
+      demand(err).be.null();
+      demand(balance).be.number();
+
+      done();
+    });
+  });
+});
